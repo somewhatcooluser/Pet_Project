2d1f9a4c7402e5f81c636a01d3a7db39e2557c15
ramen341 <rnraymond1@gmail.com>
2025-12-05 09:23:24 -0800

    Added the first brainrot and got everything to work
    simple outline for other brainrots

---
+++ Tung.java
@@ -0,0 +1,33 @@
+interface Brainrot {
+    public void eat();
+    public void sleep();
+    public void play();
+}
+public class Tung implements Brainrot{
+    private String name;
+    private double health;
+    private double drowziness;
+
+    public Tung(String name, double drowziness){
+        this.name = name;
+        this.health = 100;
+        this.drowziness = drowziness;
+        
+    }
+
+    public void eat(){
+        System.out.println("Tung Yummy");
+    }
+    public void sleep(){
+        System.out.println("zzzz");
+    }
+    public void play(){
+        System.out.println("Yay");
+    }
+
+
+    public String toString(){
+        return(name + " is at " + health + " hp and is " + drowziness + " asleep.");
+
+    }
+}

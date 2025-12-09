d626ba2687297e63f30f98ec77f8195e6a52419f
GrooovyPlanet <GrooovyPlanet@gmail.com>
2025-12-09 09:05:55 -0800

    Fixed

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
+        Brainrot brainrot = null;
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Brainrot world!");
@@ -10,16 +11,16 @@ public class Main{
         if(input.equals("e")){
             int brainrotChance = random.nextInt((100 - 1 + 1) + 1);
             if(brainrotChance <= 10){
-                Tung brainrot = new Tung("Tung", 100)
+                brainrot = new Tung("Tung", 100);
             }
             else if(brainrotChance <= 35){
-                Ballerina brainrot = new Ballerina("Ballerina", 100);
+                brainrot = new Ballerina("Ballerina", 100);
             }
             else if(brainrotChance <= 65){
-                Tralaero brainrot = new Tralaero("Tralaero", 100);
+                brainrot = new Tralalero("Tralaero", 100);
             }
             else{
-                Odin brainrot = new Odin("Odin", 100);
+                brainrot = new Odin("Odin", 100);
             }
 
 

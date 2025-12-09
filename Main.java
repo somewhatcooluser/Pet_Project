2c5487b1391166df60da12a9c0585df3ce90052d
ramen341 <rnraymond1@gmail.com>
2025-12-09 09:30:54 -0800

    bla
    s

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
+
+
 public class Main{
     public static void main(String[] args) {
         Brainrot brainrot = null;
@@ -9,7 +11,6 @@ public class Main{
         System.out.println("Type e to open an egg!");
         String input = scanner.nextLine();
         if(input.equals("e")){
-            Brainrot brainrot;
             int brainrotChance = random.nextInt((100 - 1 + 1) + 1);
             if(brainrotChance <= 10){
                 brainrot = new Tung("Tung", 100);
@@ -27,9 +28,13 @@ public class Main{
         }
         System.out.println("Congratulations! You have hatched " + (brainrot.getClass().getSimpleName()) + "!");
 
-
-
-
-
+}
+public static void displayBrainrotChances(){
+    System.out.println("Brainrot Chances:");
+    System.out.println("Tung: 10%");
+    System.out.println("Ballerina: 25%");
+    System.out.println("Tralalero: 30%");
+    System.out.println("Odin: 35%");
 }
 }
+

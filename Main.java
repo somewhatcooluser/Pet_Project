d8d5f1b17f132dca68f58ad91b3cd4fee0b0efc6
GrooovyPlanet <GrooovyPlanet@gmail.com>
2025-12-15 09:21:59 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.net.URL;
-import javax.imageio.ImageIO;
 
 public class Main{
 
@@ -63,7 +61,7 @@ public static Brainrot brainRotChances(){
 public static void mainGame(Brainrot brainrot){
     boolean game = true;
     Scanner scanner = new Scanner(System.in);
-    while( game == true && brainrot.getHealth() >= 0 && brainrot.getEnergy() >= 0){
+    while( game == true && brainrot.getHealth() > 0 && brainrot.getEnergy() > 0){
         
     System.out.println("________________");
     System.out.println(brainrot);
@@ -78,28 +76,29 @@ public static void mainGame(Brainrot brainrot){
     System.out.println("5. View Brainrot Chances");
 
     String choice = scanner.nextLine();
-        if(choice.equals("4")){
-            game = false;
-            System.out.println("Thanks for playing! Goodbye!");
-        }
-        else if(choice.equals("1")){
-            brainrot.eat();
-        }
-        else if(choice.equals("2")){
-            brainrot.sleep();
-        }
-        else if(choice.equals("3")){
-            brainrot.play();
-        }
-        else if(choice.equals("5")){
-            displayBrainrotChances();
-        }
-        else{
-            System.out.println("Invalid choice. Please try again.");
+        switch(choice){
+            case "1":
+                brainrot.eat();
+                break;
+            case "2":
+                brainrot.sleep();
+                break;
+            case "3":
+                brainrot.play();
+                break;
+            case "4":
+                game = false;
+                System.out.println("Thanks for playing! Goodbye!");
+                break;
+            case "5":
+                displayBrainrotChances();
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
         }
         System.out.println("________________");
         
-        if( brainrot.getHealth() <= 0 && brainrot.getEnergy() <= 0){
+        if( brainrot.getHealth() <= 0 || brainrot.getEnergy() <= 0){
             System.out.println("R.I.P. " + brainrot.getClass() + ". Your brainrot has passed away due to lack of care :( .");
         }
         
@@ -109,9 +108,10 @@ public static void mainGame(Brainrot brainrot){
  public static Brainrot beginning(){
     Scanner scanner = new Scanner(System.in);
     Brainrot brainrot = null;
+    String input = "e";
         System.out.println("Welcome to Brainrot world!");
-        System.out.println("Type e to open an egg!");
-        String input = scanner.nextLine();
+        System.out.println("Press enter to open an egg!");
+        scanner.nextLine();
         if(input.equals("e")){
             brainrot = brainRotChances();
         }
